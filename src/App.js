5b9071594ac8230fb8d195f00362c880e2cabb62
yaseminbadilli <yaseminbadilli@hotmail.com>
2022-06-22 20:24:02 +0300

    aa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react"
+export default class App extends React.Component{
+
+    render(){
+    
+    return (
+    
+        <div className='App'>
+        <h1>Merhaba Dünya</h1>
+     
+        </div>
+    )
+    }
+    }
